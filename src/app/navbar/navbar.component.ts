ba7e1b4a10818a7ee8ec028a10a4de9e93c6cff3
bhavyaprakash <ishapra764@gmail.com>
2025-04-03 01:01:14 +0530

    Added Angular Material and Header Component

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, MatIconModule,MatButtonModule, MatTooltipModule, MatSnackBarModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  isDarkMode = false;
+  constructor(private snackBar: MatSnackBar) {}
+
+  showDownloadMessage() {
+    this.snackBar.open('Downloading Resume...', '✖', {
+      duration: 3000, // Closes after 3 seconds
+      verticalPosition: 'top', // Positions at top
+      horizontalPosition: 'center', // Centered horizontally
+      panelClass: ['custom-snackbar'] // Custom class for styling
+    });
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('dark-theme', this.isDarkMode);
+  }
+
+}
