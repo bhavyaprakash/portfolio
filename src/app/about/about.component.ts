c8e28f92883d7a2b3bc78e22587bbfce857035de
bhavyaprakash <ishapra764@gmail.com>
2025-04-15 00:53:24 +0530

    Added Download Icon

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,13 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatChipsModule} from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { SkillsComponent } from "../skills/skills.component";
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule, MatChipsModule, MatButtonModule, MatTooltipModule, SkillsComponent],
+  imports: [CommonModule, MatChipsModule, MatButtonModule, MatTooltipModule, MatIconModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
